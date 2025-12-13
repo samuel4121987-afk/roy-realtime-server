a0d75ea40c50e142b69f4958326231f76d8ecd2a
samuel4121987-afk <samuel4121987@gmail.com>
2025-12-13 02:35:46 +0100

    Make turn detection more sensitive (threshold 0.5, silence 500ms)

--- index.js
+++ index.js
@@ -122,8 +122,7 @@ wss.on("connection", (twilioSocket) => {
         voice: "alloy",
         temperature: 0.6,
         instructions: ROY_PROMPT,
-                turn_detection: { type: "server_vad", threshold: 0.7, prefix_padding_ms: 300, silence_duration_ms: 700 },
-      },
+        turn_detection: { type: "server_vad", threshold: 0.5, prefix_padding_ms: 300, silence_duration_ms: 500 },      },
     });
 
     flushOpenAIQueue();
